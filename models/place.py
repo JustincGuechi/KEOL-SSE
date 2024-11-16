e7c1c40858c5df87af77fe399f3fe768c6099bb1
marinesable <marine.sable21@gmail.com>
2024-11-16 19:29:40 +0100

    init refacto

---
+++ models/place.py
@@ -0,0 +1,48 @@
+from enum import Enum  # Importation de la classe Enum
+import time
+
+class Type_Place(Enum):
+    RAME = 1
+    RAME_IMMOBILISEE = 2
+    NO_RAME = 3
+
+class Place:
+    def __init__(self, ligne_id, place_id, horaire_depart, horaire_arrivee,horaire_depart_bis, horaire_arrivee_bis, type_place, rame, couleur):
+        self.ligne_id = ligne_id  # Identifiant de ligne (T1, T2, 91, 92)
+        self.place_id = place_id  # Identifiant unique de la place
+        self.horaire_depart = horaire_depart  # Horaire de départ
+        self.horaire_depart_bis = horaire_depart_bis  # Horaire de départ
+        self.horaire_arrivee = horaire_arrivee  # Horaire d'arrivée
+        self.horaire_arrivee_bis = horaire_arrivee_bis  # Horaire d'arrivée
+        self.type_place = type_place  # IMO
+        self.rame = rame  # Rame associée
+        self.couleur = couleur  # Type (Lapin, Tortue)
+
+    def __repr__(self):
+        return (f"Place(ligne_id='{self.ligne_id}', place_id='{self.place_id}', "
+                f"horaire_depart='{self.horaire_depart}', horaire_arrivee='{self.horaire_arrivee}', "
+                f"type_place='{self.type_place}', rame='{self.rame}', couleur='{self.couleur}')")
+    
+    
+    def to_dict(self):
+        """Convertir l'objet Place en un dictionnaire sérialisable en JSON"""
+        return {
+            'ligne_id': self.ligne_id,
+            'place_id': self.place_id,
+            'horaire_depart': self.horaire_depart,
+            'horaire_depart_bis': self.horaire_depart_bis,
+            'horaire_arrivee': self.horaire_arrivee,
+            'horaire_arrivee_bis': self.horaire_arrivee_bis,
+            'type_place': self.type_place.name,
+            'rame': self.rame,
+            'couleur': self.couleur
+        }
+
+    '''def time_to_string(self, time_obj):
+        """Convertit un objet time en une chaîne de caractères formatée"""
+        if isinstance(time_obj, time):  # Vérifie si l'objet est bien de type time
+            return time_obj.strftime('%H:%M')  # Exemple: '08:30'
+        return str(time_obj)  # Si ce n'est pas un objet time, on le convertit en chaîne directement'''
+
+
+
---
+++ models/plan.py
@@ -0,0 +1,29 @@
+from place import Place
+class Plan:
+    def __init__(self, nom_periode):
+        self.nom_periode = nom_periode  # Nom de la période
+        self.places = [[None for _ in range(5)] for _ in range(7)]  # Tableau 2D de 7 voies et 5 rangements
+
+    def ajouter_place(self, ligne, colonne, place):
+        """
+        Ajoute un objet Place à une position précise dans le tableau 2D.
+        """
+        if not isinstance(place, Place):
+            raise TypeError("L'objet ajouté doit être une instance de la classe Place.")
+        if ligne >= len(self.places) or colonne >= len(self.places[0]):
+            raise IndexError("Les indices de ligne et colonne sont hors du tableau.")
+        
+        self.places[ligne][colonne] = place
+
+    def __repr__(self):
+        return f"Plan(nom_periode='{self.nom_periode}', places={self.places})"
+    
+    def to_dict(self):
+        """Convertir l'objet Plan en un dictionnaire sérialisable en JSON"""
+        return {
+            'nom_periode': self.nom_periode,
+            'places': [
+                [place.to_dict() if place is not None else None for place in ligne]
+                for ligne in self.places
+            ]
+        }
